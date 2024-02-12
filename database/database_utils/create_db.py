b9b16f77fc8408a11d94cf5c21d54a975cf4dfd7
Егор Храмков <egor_kuplinov@mail.ru>
2024-02-12 11:53:27 +0400

    Исправление скрипта по созданию бд

--- database/database_utils/create_db.py
+++ database/database_utils/create_db.py
@@ -1,13 +1,16 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
+from settings.db_settings import DB_SETTINGS
+
 # Устанавливаем соединение с postgres
 connection = psycopg2.connect(user="postgres", password="python")
 connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
 # Создаем курсор для выполнения операций с базой данных
+db_name = DB_SETTINGS['NAME']
 cursor = connection.cursor()
-sql_create_database = 'create database sqlalchemy_fastapi'
+sql_create_database = f'create database {db_name}'
 # Создаем базу данных
 cursor.execute(sql_create_database)
 # Закрываем соединение
