7e6063d8a113f0f92f82fe62eb2eb9bc9adec8fe
Егор Храмков <egor_kuplinov@mail.ru>
2024-02-12 17:37:27 +0400

    лишний принт

--- settings/auth_settings.py
+++ settings/auth_settings.py
@@ -4,6 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-print(SECRET_KEY)
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES')
