8cc4e426f7f6958e096b1577476b743e6ebb8f04
Егор Храмков <egor_kuplinov@mail.ru>
2024-02-15 12:56:46 +0400

    Переписывание хеширования пароля на асинхронность, добавление aiobcrypt

--- apps/auth/hash_password.py
+++ apps/auth/hash_password.py
@@ -1,13 +1,17 @@
-from passlib.context import CryptContext
+import aiobcrypt
 
 
 class HashPassword:
-    password_context = CryptContext(schemes='bcrypt', deprecated='auto')
+    """Класс для хеширования пароля"""
 
     @classmethod
-    def bcrypt(cls, password: str):
-        return cls.password_context.hash(password)
+    async def bcrypt(cls, password: str):
+        salt = await aiobcrypt.gensalt()
+        encode_password = str.encode(password)
+        hashed_password = await aiobcrypt.hashpw(encode_password, salt)
+        return hashed_password.decode()
 
     @classmethod
-    def verify(cls, hashed_password, plain_password):
-        return cls.password_context.verify(plain_password, hashed_password)
+    async def verify(cls, hashed_password, plain_password):
+        valid_password = await aiobcrypt.checkpw(plain_password, hashed_password)
+        return valid_password
--- apps/auth/oauth2.py
+++ apps/auth/oauth2.py
@@ -18,7 +18,7 @@ class OAuth2:
     @staticmethod
     def create_access_token(data: dict):
         to_encode = data.copy()
-        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.now() + timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
         to_encode.update({'exp': expire})
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt
