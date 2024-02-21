c09b87ab1f84bb8f79bfdd80792b7dde442f6a6a
Егор Храмков <egor_kuplinov@mail.ru>
2024-02-21 10:23:57 +0400

    Изменение параметров на переменные среды

--- database/database_utils/create_db.py
+++ database/database_utils/create_db.py
@@ -4,7 +4,7 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from settings.db_settings import DB_SETTINGS
 
 # Устанавливаем соединение с postgres
-connection = psycopg2.connect(user="postgres", password="python")
+connection = psycopg2.connect(user=DB_SETTINGS['USER'], password=DB_SETTINGS['PASSWORD'])
 connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
 # Создаем курсор для выполнения операций с базой данных
