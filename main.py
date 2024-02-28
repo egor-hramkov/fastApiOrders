28536ae01a57a724a6fe08bb57eb2a1eca64512f
Егор Храмков <egor_kuplinov@mail.ru>
2024-02-28 09:50:54 +0400

    отлов ошибки кафка

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from kafka.errors import NoBrokersAvailable
 
 from apps.user import routes as user_routes
 from apps.auth import routes as auth_routes
@@ -6,8 +7,6 @@ from apps.items import routes as item_routes
 from apps.orders import routes as order_routes
 from apps.notifications import routes as notifications_routes
 import concurrent.futures as pool
-
-from kafka_layer.consumer.consumer_listener import run_consumer
 
 app = FastAPI()
 
@@ -22,4 +21,8 @@ executor = pool.ThreadPoolExecutor(max_workers=1)
 
 @app.on_event("startup")
 async def startup_event():
-    executor.submit(run_consumer)
+    try:
+        from kafka_layer.consumer.consumer_listener import run_consumer
+        executor.submit(run_consumer)
+    except NoBrokersAvailable:
+        pass
